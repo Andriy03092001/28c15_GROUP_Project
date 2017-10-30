1bc7bd79771d122ec50d037d1cc3a0d6fc5f64ea
ШАГ Академія <cuanid236316@gmail.com>
2017-10-30 17:12:50 +0200

    Add Migration

---
+++ MyPract/Migrations/201710301512235_Tables.cs
@@ -0,0 +1,46 @@
+namespace MyPract.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Tables : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Games",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Players",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Teams",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Teams");
+            DropTable("dbo.Players");
+            DropTable("dbo.Games");
+        }
+    }
+}
